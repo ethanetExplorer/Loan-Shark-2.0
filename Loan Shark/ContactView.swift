2b67b342a42b1a14f78c74270dea7b1eb7239bb8
Ethan Lim <ethanolicacid@outlook.com>
2022-10-29 08:57:48 +0800

    Added Contacts struct

--- Loan Shark/ContactView.swift
+++ Loan Shark/ContactView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ContactsView: View {
+    @State var contactsList = [
+    
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(contacts) { contacts in
+            Text(contacts.name)
+        }
     }
 }
 
---
+++ Loan Shark/Contacts.swift
@@ -0,0 +1,9 @@
+//
+//  Contacts.swift
+//  Loan Shark
+//
+//  Created by Ethan Lim on 29/10/22.
+//
+
+import Foundation
+
