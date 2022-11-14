599be9778cc12f65965de3d765e910dfa9e751da
Ethan Lim <107737645+ethanetexplorer@users.noreply.github.com>
2022-11-14 11:08:30 +0800

    Added PeopleStruct

---
+++ NewTransactionSheet.swift
@@ -0,0 +1,102 @@
+//
+//  NewTransactionSheet.swift
+//  Loan Shark
+//
+//  Created by Yuhan Du Du Du Du on 6/11/22.
+//
+
+import SwiftUI
+
+struct NewTransactionSheet: View {
+    
+    @StateObject var manager = TransactionManager()
+    @State var isDetailSyncronised: Bool = false
+    @State var dueDate = Date()
+    
+    @State var transactionType = ""
+    var transactionTypes = ["Select","Loan","Bill split"]
+    @Environment(\.dismiss) var dismiss
+
+    var decimalNumberFormat: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.allowsFloats = true
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencySymbol = "$"
+        return numberFormatter
+    }
+    
+    @State var newTransaction = Transaction(name: "", people: [""], dueDate: Date.now, isPaid: false, isBillSplitTransaction: false, money: 0)
+    @Binding var transactions: [Transaction]
+
+    @State var peopleInvolved = ""
+    var contacts = ["Contact", "Dhoby Ghaut", "Bras Basah", "Esplanade", "Promenade", "Nicoll Highway", "Stadium", "Mountbatten", "Dakota", "Paya Lebar", "MacPherson", "Tai Seng", "Bartley", "Serangoon", "Lorong Chuan", "Bishan", "Marymount", "Caldecott", "Botanic Gardens", "Farrer Road", "Holland Village", "Buona Vista", "one-north", "Kent Ridge", "Haw Par Villa", "Pasir Panjang", "Labrador Park", "Telok Blangah", "HarbourFront", "Keppel", "Cantonment", "Prince Edward Road", "Marina Bay", "Bayfront"]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Transaction details")) {
+                        HStack {
+                            Text("Title")
+                            TextField("Title", text: $newTransaction.name)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        Picker("Transaction type", selection: $transactionType) {
+                            ForEach(transactionTypes, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        if transactionType == "Bill split" {
+                            Toggle(isOn: $isDetailSyncronised){
+                                Text("Syncronise details")
+                            }
+                        }
+                    }
+                    if transactionType == "Loan" {
+                        Picker("People", selection: $peopleInvolved){
+                            ForEach(contacts, id: \.self){
+                                Text($0)
+                            }
+                        }
+                        HStack {
+                            Text("Amount of money")
+                            TextField("Amount", value: $newTransaction.money, formatter: NumberFormatter())
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                    } else if transactionType == "Bill split" && isDetailSyncronised {
+                        Section {
+                            Picker("People", selection: $peopleInvolved){
+                                ForEach(contacts, id: \.self){
+                                    Text($0)
+                                }
+                            }
+                        }
+                    }
+                }
+                Button {
+                    transactions.append(newTransaction)
+                    dismiss()
+                } label: {
+                    Text("Save")
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal)
+                
+            }
+            .navigationTitle("New transaction")
+        }
+    }
+}
+
+
+struct NewTransactionSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTransactionSheet(transactions: .constant([])) 
+    }
+}
