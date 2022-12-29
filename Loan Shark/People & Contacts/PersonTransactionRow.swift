2fb33bf2b5daf552edf2115314af4b39eb07d40a
Du Yuhan <yuhan.du@gmail.com>
2022-12-30 00:21:15 +0800

    Fixed details in PersonTransactionRow being specific to the transaction and not the person

--- Loan Shark/People & Contacts/PersonTransactionRow.swift
+++ Loan Shark/People & Contacts/PersonTransactionRow.swift
@@ -30,14 +30,14 @@ struct PersonTransactionRow: View {
                 VStack(alignment: .leading) {
                     Text(transaction.name)
                         .foregroundColor(Color("PrimaryTextColor"))
-                    Text(getTransactionDueDate(for: transaction))
+                    Text(getTransactionDueDate(for: transactionPerson))
                         .font(.caption)
                         .foregroundColor(Color("SecondaryTextColor"))
                 }
                 Spacer()
                 
                 Text("$\(decimalNumberFormat.string(for: transactionPerson.money ?? 0)!)")
-                    .foregroundColor(Color(transaction.transactionStatus == .overdue ? "RadRed" : "PrimaryTextColor"))
+                    .foregroundColor(Color(transactionPerson.personTransactionStatus == .overdue ? "RadRed" : "PrimaryTextColor"))
                     .font(.title2)
             }
         }
@@ -48,17 +48,17 @@ struct PersonTransactionRow: View {
         }
     }
     
-    func getTransactionDueDate(for transaction: Transaction) -> String {
-        let dateDiff = Date.now.timeIntervalSince1970 - transaction.dueDate.timeIntervalSince1970
+    func getTransactionDueDate(for person: Person) -> String {
+        let dateDiff = Date.now.timeIntervalSince1970 - person.dueDate!.timeIntervalSince1970
         let daysAgo = abs(Int(dateDiff / (60*60*24)))
         
-        if transaction.isPaid == true {
+        if person.hasPaid == true {
             return "Paid \(daysAgo) days ago"
-        } else if daysAgo == 0 && transaction.isPaid == false {
+        } else if daysAgo == 0 && person.hasPaid == false {
             return "Due today"
-        } else if dateDiff > 0 && transaction.isPaid == false {
+        } else if dateDiff > 0 && person.hasPaid == false {
             return "Due \(daysAgo) days ago"
-        } else if dateDiff < 0 && transaction.isPaid == false {
+        } else if dateDiff < 0 && person.hasPaid == false {
             return "Due in \(daysAgo) days"
         } else {
             return ""
--- Loan Shark/TODO.md
+++ Loan Shark/TODO.md
@@ -1,10 +1,8 @@
 #BUGS & Additions:  I
--edits in transaction detail regarding due date is not reflected within home page, but editing other fields seems to refresh it
 -selectedForTransactions attribute not persisting
 -restrict transactions to > 0.01
--transaction due date in persondetailview not specific to person
 -discard changes button for editTransactionView
--Implement no due date
+-Implement no due date (GLOBAL SEARCH dueDate! to make sure everything is correct and implement exceptions if need be)
 -sort unpaid people to the top in billsplit of transaction detail view
 
 #LONG TERM GOALS: 
@@ -14,10 +12,10 @@
 -Add tags
 
 #Priority for next commit: 
+-edits in transaction detail regarding due date is not reflected within home page, but editing other fields seems to refresh it
+-change the weird hours minutes thing
 
 
 #Fixed: 
--done button is still there despite there being no keyboard
--editTransactionView amount field being empty by default
--Added scroll away keyboard
+-transaction due date in persondetailview not specific to person
 
